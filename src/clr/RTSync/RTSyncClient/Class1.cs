7404f04dbe8ec8aa8bba08ffbec1b7845187cdc2
David Parker <davidgparker123@utexas.edu>
2020-04-11 16:07:01 -0700

    rtsync

---
+++ src/clr/RTSync/RTSyncClient/Class1.cs
@@ -0,0 +1,16 @@
+﻿using CitizenFX.Core;
+
+namespace RTSync.Client
+{
+    public class ClientHandler : BaseScript
+    {
+        public ClientHandler()
+        {
+            TriggerEvent("chat:addMessage", new
+            {
+                color = new[] { 255, 0, 0 },
+                args = new[] { "[RTSync]", $"Hello, World." }
+            });
+        }
+    }
+}
